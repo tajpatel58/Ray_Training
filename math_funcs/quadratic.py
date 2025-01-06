dc0e2836e310d41b31a525bbe03f7f9e9b67fb79
Taj Patel <tajpatel58@gmail.com>
2025-01-06 21:37:04 +0000

    initial commit

---
+++ math_funcs/quadratic.py
@@ -0,0 +1,4 @@
+
+
+def quadratic_curve(a: float, b:float, c: float, x: float) -> float:
+    return a * x ** 2 + b * x + c 
---
+++ testing/test_quadratic.py
@@ -0,0 +1,30 @@
+from ..math_funcs.quadratic import quadratic_curve
+from pathlib import Path
+import random
+import numpy as np
+import ray
+
+
+def generate_y_coordinates(a: float, 
+                           b: float,
+                           c: float, 
+                           x_coords: list,
+                           use_ray: bool) -> list:
+    if use_ray:
+        wheel_path = Path(__file__).resolve().parent / "dist" / "tp_math-0.1.0-py3-none-any.whl"
+        print(wheel_path)
+        ray.init(ignore_reinit_error=True,
+                 runtime_env={"py_modules" : [str(wheel_path)]})
+        
+        @ray.remote
+        def quadratic_curve_ray(x: float) -> float:
+            return quadratic_curve(a, b, c, x)
+        
+
+        tasks = [quadratic_curve_ray(x_i) for x_i in x_coords]
+        results = ray.get(tasks)
+        ray.shutdown()
+        return results
+
+    else:
+        return [quadratic_curve(a, b, c, x_i) for x_i in x_coords]
